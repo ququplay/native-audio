56f89710de9f6fbf3f114a22b507943e7fc548f3
Michal Kuklis <michal.kuklis@gmail.com>
2026-04-06 23:32:46 -0400

    Adjust package

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "CapacitorCommunityNativeAudio",
+    name: "QuquplayNativeAudio",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "CapacitorCommunityNativeAudio",
+            name: "QuquplayNativeAudio",
             targets: ["NativeAudio"])
     ],
     dependencies: [
